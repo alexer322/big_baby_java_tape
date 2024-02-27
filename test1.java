87db09bca1d3fd391091a84af7ddc20cf8878882
Alexandr Erofeev <alexerofeevnn@gmail.com>
2024-02-27 18:19:47 +0300

    added first task

---
+++ test1.java
@@ -0,0 +1,10 @@
+public class test1 {
+    public static void main(String[] args) {
+        int God = 3126;
+        int a = 8;
+        int result = God - a;
+
+        String god = "Сейчас";
+        System.out.print(god +" "+ result);
+    }
+}
